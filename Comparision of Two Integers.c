a08910a4d2faab9d6f63c123a5d3eba21fe75176
arjunabalpande06 <arjunabalpande@gmail.com>
2026-01-06 18:58:55 +0530

    Create Comparision of Two Integers.c

---
+++ Comparision of Two Integers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int num1;
+	int num2;
+	printf("Enter the first integer: ");
+	scanf("%d",&num1);
+	printf("Enter the second integer: ");
+	scanf("%d",&num2);
+	if (num1>num2){
+		printf("%d is greater than %d\n",num1,num2);
+	}
+	else {
+		if(num1<num2){
+			printf("%d is less than %d\n",num1,num2);}
+		else {
+			printf("%d is equal to %d\n",num1,num2);
+		}
+	}
+		
+}
